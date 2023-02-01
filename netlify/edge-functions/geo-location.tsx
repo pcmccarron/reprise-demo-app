3614289c2c3b38ddd1c3cd48941b8f52e400874e
Peter McCarron <pcmccarron@gmail.com>
2023-02-01 15:37:49 -0700

    seeing if this fixes the function

--- netlify/edge-functions/geo-location.tsx
+++ netlify/edge-functions/geo-location.tsx
@@ -2,6 +2,6 @@ import type { Config, Context } from 'https://edge.netlify.com/';
 
 export default async (request: Request, context: Context) => {
 
-    return new Response(context.geo.city);
+    return new Response(`The location is ${context.geo.city}`);
 }
 export const config: Config = {path: "/city"}
